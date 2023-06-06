0d350494e0a249bcf8dba88032b1a60823ccddb9
Ashurov Georgy <ashurov1308@mail.ru>
2023-06-06 16:57:57 +0300

    add requests create, get, get one, update, delete for ventilated facades

--- controllers/ventilated_facades.controller.js
+++ controllers/ventilated_facades.controller.js
@@ -5,23 +5,31 @@ class VentilatedFacadesController {
         const {ventilatedFacadesTitle, ventilatedFacadesUrl} = req.body
         const newVentilatedFacades = await db.query('INSERT INTO ventilated_facades(ventilated_facades_title, ventilated_facades_url) VALUES ($1, $2) RETURNING *',
         [ventilatedFacadesTitle, ventilatedFacadesUrl])
-        res.json(newVentilatedFacades)
+        res.json(newVentilatedFacades.rows[0]) // Возвращаем только добавленный элемент
     }
 
     async getVentilatedFacades(req, res) {
-        
+        const ventilatedFacades = await db.query('SELECT * FROM ventilated_facades')
+        res.json(ventilatedFacades.rows) // Возвращаем весь select
     }
 
     async getOneVentilatedFacade(req, res) {
-        
+        const id = req.params.id //id - из url страницы
+        const ventilatedFacades = await db.query('SELECT * FROM ventilated_facades where ventilated_facades_id = $1', [id])
+        res.json(ventilatedFacades.rows[0]) // Возвращаем полученную карточку
     }
 
     async updateVentilatedFacade(req, res) {
-        
+        const {ventilatedFacadesId, ventilatedFacadesTitle, ventilatedFacadesUrl} = req.body
+        const ventilatedFacades = await db.query('UPDATE ventilated_facades set ventilated_facades_title = $1, ventilated_facades_url = $2 where ventilated_facades_id = $3 RETURNING *',
+        [ventilatedFacadesTitle, ventilatedFacadesUrl, ventilatedFacadesId])
+        res.json(ventilatedFacades.rows[0])
     }
     
     async deleteVentilatedFacade(req, res) {
-        
+        const id = req.params.id //id - из url страницы
+        const ventilatedFacades = await db.query('DELETE FROM ventilated_facades where ventilated_facades_id = $1', [id])
+        res.json(ventilatedFacades.rows[0]) // Возвращаем полученную карточку
     }
 }
 
