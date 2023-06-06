5a91b9b96f7ef95b64f87825499c01d55def4d40
Ashurov Georgy <ashurov1308@mail.ru>
2023-06-06 16:12:39 +0300

    add creating new ventlated facades item

--- controllers/ventilated_facades.controller.js
+++ controllers/ventilated_facades.controller.js
@@ -1,6 +1,11 @@
+const db = require('../db')
+
 class VentilatedFacadesController {
     async createVentilatedFacade(req, res) {
-
+        const {ventilatedFacadesTitle, ventilatedFacadesUrl} = req.body
+        const newVentilatedFacades = await db.query('INSERT INTO ventilated_facades(ventilated_facades_title, ventilated_facades_url) VALUES ($1, $2) RETURNING *',
+        [ventilatedFacadesTitle, ventilatedFacadesUrl])
+        res.json(newVentilatedFacades)
     }
 
     async getVentilatedFacades(req, res) {
--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const ventilatedFacadesRouter = require('./routes/ventilatedFacades.routes')
 const PORT = process.env.POST || 8000 // Берет порт окружения
 
 const app = express()
-
+app.use(express.json()) // Так как express не может по умолчанию распарсить json строку
 app.use('/api', ventilatedFacadesRouter)
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
