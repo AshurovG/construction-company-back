aceb4384af4167a03b1e373571bb215b66b23df4
Ashurov Georgy <ashurov1308@mail.ru>
2023-06-07 23:06:14 +0300

    before add new architecture

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express')
+// const path = require('path')
+
 const ventilatedFacadesRouter = require('./routes/ventilated_facades.routes')
 const ventilatedFacadeItemsRouter = require('./routes/ventilated_facade_items.routes')
 const exteriorDesignRouter = require('./routes/exterior_design.routes')
@@ -7,6 +9,10 @@ const exteriorDesignItemsRouter = require('./routes/exterior_design_items.routes
 const PORT = process.env.POST || 8000 // Берет порт окружения
 
 const app = express()
+// app.use(express.static(path.resolve(__dirname, 'client')))
+// app.get('*', (req, res) => {
+//   res.redirect('https://frolfasd.netlify.app/?anch=faqs');
+// });
 app.use(express.json()) // Так как express не может по умолчанию распарсить json строку
 app.use('/api', ventilatedFacadesRouter)
 app.use('/api', ventilatedFacadeItemsRouter)
