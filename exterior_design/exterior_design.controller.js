aef2cc3bd3155cb428a28792eee56df342206dff
Ashurov Georgy <ashurov1308@mail.ru>
2023-06-10 17:16:31 +0300

    correct controller for exterior design

--- exterior_design/exterior_design.controller.js
+++ exterior_design/exterior_design.controller.js
@@ -1,8 +1,10 @@
 const db = require('../db')
+const {ExteriorDesignDAO} = require('./exterior_design.DAO')
 
 class ExteriorDesignController {
-    async getExteriorDesign(req, res) {
-        ventilatedFacadesDAO.getAll()
+    createExteriorDesign(req, res) {
+        const {title, url} = req.body
+        ExteriorDesignDAO.insertNew(title, url)
             .then((data) => {
                 res.json(data)
             })
@@ -15,62 +17,70 @@ class ExteriorDesignController {
             });
     }
 
-
-    // async createExteriorDesign(req, res) {
-    //     try
-    //     {
-    //         const {exteriorDesignTitle, exteriorDesignUrl} = req.body
-    //         const newExteriorDesign = await db.query('INSERT INTO exterior_design(exterior_design_title, exterior_design_url) VALUES ($1, $2) RETURNING *',
-    //         [exteriorDesignTitle, exteriorDesignUrl])
-    //         res.json(newExteriorDesign.rows[0]) // Возвращаем только добавленный элемент
-    //     } catch(err) {
-    //         res.status(400).send({status: 'Bad Request', message: err.message})
-    //     }
-    // }
-
-    // async getExteriorDesign(req, res) {
-    //     try
-    //     {
-    //         const exteriorDesign = await db.query('SELECT * FROM exterior_design')
-    //         res.json(exteriorDesign.rows) // Возвращаем весь select
-    //     } catch(err) {
-    //         res.status(400).send({status: 'Bad Request', message: err.message})
-    //     }
-    // }
-
-    // async getOneExteriorDesign(req, res) {
-    //     try
-    //     {
-    //     const id = req.params.id //id - из url страницы
-    //     const exteriorDesign = await db.query('SELECT * FROM exterior_design where exterior_design_id = $1', [id])
-    //     res.json(exteriorDesign.rows[0]) // Возвращаем полученную карточку
-    //     } catch(err) {
-    //         res.status(400).send({status: 'Bad Request', message: err.message})
-    //     }
-    // }
-
-    // async updateExteriorDesign(req, res) {
-    //     try
-    //     {
-    //         const {exteriorDesignId, exteriorDesignTitle, exteriorDesignUrl} = req.body
-    //         const exteriorDesign = await db.query('UPDATE exterior_design set exterior_design_title = $1, exterior_design_url = $2 where exterior_design_id = $3 RETURNING *',
-    //         [exteriorDesignTitle, exteriorDesignUrl, exteriorDesignId])
-    //         res.json(exteriorDesign.rows[0])
-    //     } catch(err) {
-    //         res.status(400).send({status: 'Bad Request', message: err.message})
-    //     }
-    // }
+    async getExteriorDesign(req, res) {
+        ExteriorDesignDAO.getAll()
+            .then((data) => {
+                res.json(data)
+            })
+            .catch((error) => {
+                if (error.status === 500) {
+                    res.status(500).send({status: 'Problem', message: 'Problem with database'})
+                } else {
+                    res.status(400).send({status: 'Bad Request', message: error.message})
+                }
+            });
+    }
     
-    // async deleteExteriorDesign(req, res) {
-    //     try
-    //     {
-    //         const id = req.params.id //id - из url страницы
-    //         const exteriorDesign = await db.query('DELETE FROM exterior_design where exterior_design_id = $1', [id])
-    //         res.json(exteriorDesign.rows[0]) // Возвращаем полученную карточку
-    //     } catch(err) {
-    //         res.status(400).send({status: 'Bad Request', message: err.message})
-    //     }
-    // }
+    async getOneExteriorDesign(req, res) {
+        const id = req.params.id //id - из url страницы
+        ExteriorDesignDAO.getById(id)
+            .then((data) => {
+                res.json(data)
+            })
+            .catch((error) => {
+                if (error.status === 404) {
+                    res.status(error.status).send({status: 'Not found', message: error.message})
+                } else if (error.status === 500) {
+                    res.status(500).send({status: 'Problem', message: 'Problem with database'})
+                } else {
+                    res.status(400).send({status: 'Bad Request', message: error.message})
+                }
+            });
+    }
+
+    async deleteExteriorDesign(req, res) {
+        const id = req.params.id //id - из url страницы
+        ExteriorDesignDAO.deleteById(id)
+            .then((data) => {
+                res.json(data)
+            })
+            .catch((error) => {
+                if (error.status === 404) {
+                    res.status(error.status).send({status: 'Not found', message: error.message})
+                } else if (error.status === 500) {
+                    res.status(500).send({status: 'Problem', message: 'Problem with database'})
+                } else {
+                    res.status(400).send({status: 'Bad Request', message: error.message})
+                }
+            });
+    }
+
+    async updateExteriorDesign(req, res) {
+        const {id, title, url} = req.body
+        ExteriorDesignDAO.updateById(id, title, url)
+            .then((data) => {
+                res.json(data)
+            })
+            .catch((error) => {
+                if (error.status === 404) {
+                    res.status(error.status).send({status: 'Not found', message: error.message})
+                } else if (error.status === 500) {
+                    res.status(500).send({status: 'Problem', message: 'Problem with database'})
+                } else {
+                    res.status(400).send({status: 'Bad Request', message: error.message})
+                }
+            });
+    }
 }
 
 module.exports = new ExteriorDesignController()
--- exterior_design/exterior_design.routes.js
+++ exterior_design/exterior_design.routes.js
@@ -2,10 +2,10 @@ const Router = require('express')
 const router = new Router()
 const exteriorDesignController = require('./exterior_design.controller')
 
-// router.post('/exteriorDesign', exteriorDesignController.createExteriorDesign)
+router.post('/exteriorDesign', exteriorDesignController.createExteriorDesign)
 router.get('/exteriorDesign', exteriorDesignController.getExteriorDesign)
-// router.get('/exteriorDesign/:id', exteriorDesignController.getOneExteriorDesign)
-// router.put('/exteriorDesign', exteriorDesignController.updateExteriorDesign) //обновление данных
-// router.delete('/exteriorDesign/:id', exteriorDesignController.deleteExteriorDesign)
+router.get('/exteriorDesign/:id', exteriorDesignController.getOneExteriorDesign)
+router.put('/exteriorDesign', exteriorDesignController.updateExteriorDesign) //обновление данных
+router.delete('/exteriorDesign/:id', exteriorDesignController.deleteExteriorDesign)
 
 module.exports = router
