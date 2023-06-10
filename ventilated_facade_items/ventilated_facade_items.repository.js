6f0f9e6188f4ae82e78981a1cd3682d0e1311627
Ashurov Georgy <ashurov1308@mail.ru>
2023-06-10 17:31:37 +0300

    add repository for ventilated facade items

---
+++ ventilated_facade_items/ventilated_facade_items.repository.js
@@ -0,0 +1,45 @@
+const db = require('../db')
+class VentilatedFacadesItemsRepository {
+    static insertNew(url, id) {    
+        return new Promise((resolve, reject) => {
+            db.query('INSERT INTO ventilated_facade_items(ventilated_facade_items_url, ventilated_facades_id) VALUES ($1, $2) RETURNING *', [url, id], (error, results) => {
+                if (error) {
+                    reject(error);
+                } else {
+                    const data = results.rows[0];
+                    resolve(data);
+                }
+            });
+        });
+    }
+
+    static getAll(id) {
+        return new Promise((resolve, reject) => {
+            db.query('SELECT * FROM ventilated_facade_items WHERE ventilated_facades_id = $1', [id], (error, results) => {
+                if (error) {
+                    reject(error);
+                } else {
+                    const data = results.rows;
+                    resolve(data);
+                }
+            });
+        });
+    }
+
+    static deleteById(idOne, idMany) {
+        return new Promise((resolve, reject) => {
+            db.query('DELETE FROM ventilated_facade_items WHERE ventilated_facades_id = $1 AND ventilated_facade_items_id = $2', [idOne, idMany], (error, results) => {
+              if (error) {
+                reject(error);
+              } else {
+                const data = results.rows[0];
+                resolve(data);
+              }
+            });
+          });
+    }
+}
+
+module.exports = {
+    VentilatedFacadesItemsRepository
+}
