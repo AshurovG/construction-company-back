9ffb0972cbd6dde0c3f161269b7916855ae6d2b6
Ashurov Georgy <ashurov1308@mail.ru>
2023-08-12 15:51:08 +0300

    delete function isExistsIdMany because it calls errors for undefined ventilated facades items from ventilated facade

--- ventilated_facade_items/ventilated_facade_items.DAO.js
+++ ventilated_facade_items/ventilated_facade_items.DAO.js
@@ -1,4 +1,4 @@
-const {VentilatedFacadesItemsRepository} = require('./ventilated_facade_items.repository')
+const { VentilatedFacadesItemsRepository } = require('./ventilated_facade_items.repository')
 
 class VentilatedFacadesItemsDAO {
     constructor(id, url) {
@@ -15,43 +15,43 @@ class VentilatedFacadesItemsDAO {
     }
 
     static async _validate(ventilatedFacade) { // Проверка на определенность каждого параметра
-        if (await (ventilatedFacade.url=== undefined ||
+        if (await (ventilatedFacade.url === undefined ||
             ventilatedFacade.ventilatedFacadeId === undefined)
         ) {
             let error = new Error('invalidate ventilated facade items data');
             error.status = 400
-            throw  error
+            throw error
         }
     }
 
     static async isExistsIdOneAndIdMany(idOne, idMany) { // Проверка на наличие обоих индексов в таблице
         if (await VentilatedFacadesItemsRepository.getByIdOneToMany(idOne, idMany) === undefined) {
-            let error = new Error('no such ventilated_facades_id or ventilated_facades_id found')
+            let error = new Error(`no such ventilated_facades_id or ventilated_facades_id found id=${idMany}`)
             error.status = 404
             throw error
         }
     }
 
-    static async isExistsIdMany(idMany) { // Проверка на наличие внешнего ключа в таблице
-        if (await VentilatedFacadesItemsRepository.getByIdMany(idMany) === undefined) {
-            let error = new Error('no such ventilated_facades_id found')
-            error.status = 404
-            throw error
-        }
-    }
+    // static async isExistsIdMany(idMany) { // Проверка на наличие внешнего ключа в таблице
+    //     if (await VentilatedFacadesItemsRepository.getByIdMany(idMany) === undefined) {
+    //         let error = new Error('no such ventilated_facades_id found')
+    //         error.status = 404
+    //         throw error
+    //     }
+    // }
 
     static async insertNew(url, ventilatedFacadeId) {
-        await this._validate({url, ventilatedFacadeId})
+        await this._validate({ url, ventilatedFacadeId })
         return await VentilatedFacadesItemsRepository.insertNew(url, ventilatedFacadeId)
     }
 
     static async getAll(ventilatedFacadeId) {
         try {
             await this._validateId(ventilatedFacadeId)
-            await this.isExistsIdMany(ventilatedFacadeId)
+            // await this.isExistsIdMany(ventilatedFacadeId)
             const query = await VentilatedFacadesItemsRepository.getAll(ventilatedFacadeId)
             return query
-        } catch(error) {
+        } catch (error) {
             throw error
         }
     }
@@ -63,10 +63,10 @@ class VentilatedFacadesItemsDAO {
             await this.isExistsIdOneAndIdMany(idOne, idMany)
             const query = await VentilatedFacadesItemsRepository.deleteById(idOne, idMany)
             return query
-        } catch(error) {
+        } catch (error) {
             throw error
         }
-        
+
     }
 }
 
