f097aef84f86998c7cc092328f0b739a7be9ff58
Ashurov Georgy <ashurov1308@mail.ru>
2023-08-20 20:17:49 +0300

    edit exterior design DAO

--- exterior_design_items/exterior_design_items.DAO.js
+++ exterior_design_items/exterior_design_items.DAO.js
@@ -1,4 +1,4 @@
-const {ExteriorDesignItemsRepository} = require('./exterior_design_items.repository')
+const { ExteriorDesignItemsRepository } = require('./exterior_design_items.repository')
 
 class ExteriorDesignItemsDAO {
     constructor(id, url) {
@@ -15,18 +15,26 @@ class ExteriorDesignItemsDAO {
     }
 
     static async _validate(exteriorDesign) { // Проверка на определенность каждого параметра
-        if (await (exteriorDesign.url=== undefined ||
+        if (await (exteriorDesign.url === undefined ||
             exteriorDesign.exteriorDesignId === undefined)
         ) {
             let error = new Error('invalidate exterior design items data');
             error.status = 400
-            throw  error
+            throw error
         }
     }
 
     static async isExistsIdOneAndIdMany(idOne, idMany) { // Проверка на наличие обоих индексов в таблице
         if (await ExteriorDesignItemsRepository.getByIdOneToMany(idOne, idMany) === undefined) {
             let error = new Error('no such exterior_design_id or exterior_design_items_id found')
+            error.status = 404
+            throw error
+        }
+    }
+
+    static async isExistsId(id) { // Проверка на наличие этого индекса в таблице
+        if (await ExteriorDesignItemsRepository.getById(id) === undefined) {
+            let error = new Error(`no such id found. id=${id}`)
             error.status = 404
             throw error
         }
@@ -42,7 +50,7 @@ class ExteriorDesignItemsDAO {
 
     static async insertNew(url, exteriorDesignId) {
         console.log(exteriorDesignId)
-        await this._validate({url, exteriorDesignId})
+        await this._validate({ url, exteriorDesignId })
         return await ExteriorDesignItemsRepository.insertNew(url, exteriorDesignId)
     }
 
@@ -52,7 +60,18 @@ class ExteriorDesignItemsDAO {
             await this.isExistsIdMany(exteriorDesignId)
             const query = await ExteriorDesignItemsRepository.getAll(exteriorDesignId)
             return query
-        } catch(error) {
+        } catch (error) {
+            throw error
+        }
+    }
+
+    static async getById(id) {
+        try {
+            await this._validateId(id)
+            await this.isExistsId(id)
+            const query = await ExteriorDesignItemsRepository.getById(id)
+            return query
+        } catch (error) {
             throw error
         }
     }
@@ -62,12 +81,18 @@ class ExteriorDesignItemsDAO {
             await this._validateId(idOne) // Проверяем каждый индекс по отдельности
             await this._validateId(idMany)
             await this.isExistsIdOneAndIdMany(idOne, idMany)
+            const exteriorDesignItem = await this.getById(idMany)
+            const fileUrl = exteriorDesignItem.exterior_design_items_url
+            const newUrl = fileUrl.substring(fileUrl.indexOf("static"));
+            fs.unlink(newUrl, () => { // Для удаления самих файлов картинок
+                return
+            })
             const query = await ExteriorDesignItemsRepository.deleteById(idOne, idMany)
             return query
-        } catch(error) {
+        } catch (error) {
             throw error
         }
-        
+
     }
 }
 
