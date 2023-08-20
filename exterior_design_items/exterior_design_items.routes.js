d22ddc6aec17c355d188f40d4b9d0b0b740f9615
Ashurov Georgy <ashurov1308@mail.ru>
2023-08-20 20:24:14 +0300

    edit exterior design items routes

--- exterior_design_items/exterior_design_items.routes.js
+++ exterior_design_items/exterior_design_items.routes.js
@@ -1,8 +1,19 @@
-const Router = require('express')
-const router = new Router()
+const express = require('express');
+const router = express.Router();
+const multer = require('multer');
+
 const exteriorDesignItemsController = require('./exterior_design_items.controller')
 
-router.post('/exteriorDesignitems', exteriorDesignItemsController.createExteriorDesignItem)
+const MAX_SIZE = 2000000
+
+const upload = multer({
+    dest: 'files/',
+    limits: {
+        fileSize: MAX_SIZE
+    }
+})
+
+router.post('/exteriorDesignitems', upload.single('file'), exteriorDesignItemsController.createExteriorDesignItem)
 router.get('/exteriorDesignitems/:id', exteriorDesignItemsController.getExteriorDesignItemsFromOneExteriorDesign)
 router.delete('/exteriorDesignitems/:id', exteriorDesignItemsController.deleteExteriorDesignItem)
 
