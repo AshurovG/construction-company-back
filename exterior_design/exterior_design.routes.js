b18c14966706dc6089dc76d88e8cf88d94156c9b
Ashurov Georgy <ashurov1308@mail.ru>
2023-08-20 17:47:37 +0300

    change router file for exterior design

--- exterior_design/exterior_design.routes.js
+++ exterior_design/exterior_design.routes.js
@@ -1,11 +1,22 @@
 const Router = require('express')
 const router = new Router()
+const multer = require('multer');
+
 const exteriorDesignController = require('./exterior_design.controller')
 
-router.post('/exteriorDesign', exteriorDesignController.createExteriorDesign)
+const MAX_SIZE = 2000000
+
+const upload = multer({
+    dest: 'files/',
+    limits: {
+        fileSize: MAX_SIZE
+    }
+})
+
+router.post('/exteriorDesign', upload.single('file'), exteriorDesignController.createExteriorDesign)
 router.get('/exteriorDesign', exteriorDesignController.getExteriorDesign)
 router.get('/exteriorDesign/:id', exteriorDesignController.getOneExteriorDesign)
-router.put('/exteriorDesign', exteriorDesignController.updateExteriorDesign) //обновление данных
+router.put('/exteriorDesign', upload.single('file'), exteriorDesignController.updateExteriorDesign) //обновление данных
 router.delete('/exteriorDesign/:id', exteriorDesignController.deleteExteriorDesign)
 
 module.exports = router
