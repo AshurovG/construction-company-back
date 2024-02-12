652c7e129376d6afe7bd4afc660ce83e60e0c6b0
root <root@194-67-74-216.cloudvps.regruhosting.ru>
2024-02-12 16:49:22 +0300

    finish send email request

--- email/email.controller.js
+++ email/email.controller.js
@@ -6,31 +6,24 @@ dotenv.config()
 const mailgun = new Mailgun(formData);
 const client = mailgun.client({ username: 'api', key: process.env.MAILGUN_API_KEY });
 
-// const data = {
-//   from: 'Excited User <ashurov1308@gmail.com>',
-//   to: 'ashurov.g13@gmail.com',
-//   subject: 'Заказ на постройку здания',
-//   text: 'Тема заказа в !'
-// };
-
-// client.messages.create(process.env.MAILGUN_DOMAIN, data)
-//   .then((response) => {
-//     console.log(response);
-//   })
-//   .catch((error) => {
-//     console.error(error);
-//   });
-
 class EmailController {
     async sendEmail(req, res) {
         try {
             const {fio, email, description } = req.body;
             res.send({message: 'success'})
+
+            const htmlContent = `
+                <h1>Новое обращение с сайта!</h1>
+                <p>ФИО отправителя: ${fio}</p>
+                <p>E-mail отправителя: ${email}</p>
+                <p>Описание заказа: ${description}</p>
+            `;
+
             const data = {
                 from: '<ashurov1308@gmail.com>',
                 to: 'ashurov.g13@gmail.com',
                 subject: 'Новое обращение с сайта!',
-                text: `ФИО: ${fio}. E-mail отправителя: ${email}. Описание заказа: ${description}`
+                html: htmlContent
             };
 
             client.messages.create(process.env.MAILGUN_DOMAIN, data)
@@ -45,4 +38,5 @@ class EmailController {
         }
     }
 }
+
 module.exports = new EmailController()
